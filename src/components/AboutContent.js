f539c79c20051fdbe1fdec4a8f09d681ac0545cd
Saravanan S <ssslifeskills@gmail.com>
2022-09-01 14:58:29 +0530

    styles

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -14,7 +14,7 @@ const AboutContent = () => {
   </p>
   <p>
   I am Good at Communicating with Clients or others within a Team and getting things done from myself as well as others.
-  Here's how, After My Graduation, I worked for more than 2 years ( July 2018 - Jan 2021 ) as an Electrical Engineer to help Civil Engineers in building Constructions at SOBHA Ltd.
+  Here's how, After My Graduation, I worked for more than 2 years  ( July 2018 - Jan 2021 ) as an Electrical Engineer to help Civil Engineers in building Constructions at SOBHA Ltd.
   There I Led a team of 17+ Technicians in 2 Different Projects.
   Then I worked as a Senior Engineer for 9 Months ( Jan 2021 - Oct 2021 ) in Solving On-Site Clients Problems at ZOOMLION Ltd. 
   Then I started Learning Web Development in a BootCamp and Building Projects.
