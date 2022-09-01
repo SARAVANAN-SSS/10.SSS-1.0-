2e703dd7f6aef4071e89e0df1e7fefaafd668bf8
Saravanan S <ssslifeskills@gmail.com>
2022-09-01 16:53:38 +0530

    resume added

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -10,7 +10,7 @@ const HomeContent = () => {
       <p>SARAVANAN S</p>
       <h1>Full Stack Developer</h1>
       <a href="https://www.linkedin.com/in/saravanan-s-full-stack-developer/" className="btn" target="_blank" rel="noreferrer">Linkedin</a>
-      <a href="https://drive.google.com/file/d/1WZ5HxEc8CThpuvFcI_6XlZOI1MpO8kKA/view?usp=sharing" className="btn btn-light" target="_blank" rel="noreferrer">Resume</a>
+      <a href="https://drive.google.com/file/d/1sms6ZCvWdMnG8HWuhG1i1k7dQ_fjjT3S/view?usp=sharing" className="btn btn-light" target="_blank" rel="noreferrer">Resume</a>
       <a href="https://github.com/SARAVANAN-SSS" className="btn" target="_blank" rel="noreferrer">Github</a>
     </div>
   </div>
