b36c9968c027681957c37774fb4e6efcef39f88f
Saravanan S <ssslifeskills@gmail.com>
2022-09-01 14:45:12 +0530

    edit

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -13,10 +13,10 @@ const AboutContent = () => {
   I Believe in the Principle of KISS ( Keep It Simple Stupid ), so that users can have a Great Experience. 
   </p>
   <p>
-  I am Good at Communicating with Clients or others within a Team and getting things done from Myself as well as others.
-  Here's how, After My Graduation, I worked for More than 2 years ( July 2018 - Jan 2021 ) as an Electrical Engineer to help Civil Engineers in building Construction at SOBHA Ltd.
-  There I Led a team of 17+ Technicians on 2 Different Projects.
-  Then I worked as a Senior Engineer for 9 Months (Jan 2021 - Oct 2021 ) in Solving On-Site Clients Problems at ZOOMLION Ltd. 
+  I am Good at Communicating with Clients or others within a Team and getting things done from myself as well as others.
+  Here's how, After My Graduation, I worked for more than 2 years ( July 2018 - Jan 2021 ) as an Electrical Engineer to help Civil Engineers in building Constructions at SOBHA Ltd.
+  There I Led a team of 17+ Technicians in 2 Different Projects.
+  Then I worked as a Senior Engineer for 9 Months ( Jan 2021 - Oct 2021 ) in Solving On-Site Clients Problems at ZOOMLION Ltd. 
   Then I started Learning Web Development in a BootCamp and Building Projects.
   Check Out My Projects <Link className="links" to="/projects">Here</Link> and 
   Lemme Know How I can help you <Link className="links" to="/contact">Here</Link>.
