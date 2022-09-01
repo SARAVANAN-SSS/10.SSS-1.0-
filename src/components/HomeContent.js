dbd6ff2cf7a01a3b5f08905bc9c947b03ecf9b0f
Saravanan S <ssslifeskills@gmail.com>
2022-09-01 12:38:10 +0530

    Edits

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -8,13 +8,14 @@ const HomeContent = () => {
   <div className="home">
     <div className="content">
       <p>SARAVANAN S</p>
-      <h1>FullStack Developer</h1>
-      <a href="https://www.linkedin.com/in/saravanan-s-325ab7236/" className="btn" target="_blank" rel="noreferrer">Linkedin</a>
+      <h1>Full Stack Developer</h1>
+      <a href="https://www.linkedin.com/in/saravanan-s-full-stack-developer/" className="btn" target="_blank" rel="noreferrer">Linkedin</a>
       <a href="https://drive.google.com/file/d/1WZ5HxEc8CThpuvFcI_6XlZOI1MpO8kKA/view?usp=sharing" className="btn btn-light" target="_blank" rel="noreferrer">Resume</a>
       <a href="https://github.com/SARAVANAN-SSS" className="btn" target="_blank" rel="noreferrer">Github</a>
     </div>
   </div>
   );
 };
+
 
 export default HomeContent;
